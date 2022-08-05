89b0e02363b11349260c09d0e4096412ff614881
yWoods78 <78935973+yWoods78@users.noreply.github.com>
2022-08-05 20:51:28 -0300

    identação

--- server.lua
+++ server.lua
@@ -8,8 +8,8 @@ function woo.pagamento()
 	local source = source
 	local user_id = vRP.getUserId(source)
 	if user_id then
-        randmoney = (math.random(500,750))
-	    vRP.giveMoney(user_id,parseInt(randmoney))
+        	randmoney = (math.random(500,750))
+	    	vRP.giveMoney(user_id,parseInt(randmoney))
 		TriggerClientEvent("Notify",source, "sucesso","Você recebeu <b>$"..vRP.format(parseInt(randmoney)).." reais</b>.", 5000)
 	end
 end
